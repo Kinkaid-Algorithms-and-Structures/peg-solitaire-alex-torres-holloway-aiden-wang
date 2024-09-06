0e3d32bb3250746ac533567f203a7e0db0e15a6d
aidenwang1 <aiden.yuxuan@gmail.com>
2024-09-06 08:38:14 -0500

    Added type hints

--- Peg.py
+++ Peg.py
@@ -3,14 +3,8 @@ class Peg:
         self.x = x
         self.y = y
 
-    def get_pos(self):
-        return (self.x, self.y)
+    def get_pos(self) -> list:
+        return [self.x, self.y]
 
-    def legal_move(self):
-        pass
-        # if peg has neighbor
-        #   if peg does not have neighbor
-                #return true
-
-    def has_neighbors(self):
+    def has_neighbors(self) -> bool:
         pass
